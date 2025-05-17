67e4cfcab5d52fce43ef49b0a2d0b0d0a33a7e61
Jane Duke <bastdag@gmail.com>
2025-05-17 09:02:11 -0400

    Upadate v6.6.6

--- Infinity.cpp
+++ Infinity.cpp
@@ -5,13 +5,13 @@ int main()
     while(true)
     {
     bool truth = 1;
-    bool infinity = truth + 1;
+    bool Infinity = truth + 1;
     if(0)
     {
         1;
     }
-    cin >> infinity;
-    cout << infinity;
+    cin >> Infinity;
+    cout << Infinity;
     }
     return true;
 }
